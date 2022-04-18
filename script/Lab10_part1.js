156581d01b709f36e3c7a873f0c2387d17fda760
khann97 <khann7@student.douglascollege.ca>
2022-04-18 14:15:18 -0700

    first commit

---
+++ script/Lab10_part1.js
@@ -0,0 +1,11 @@
+
+function backgroundChangeColor(colorBackground){
+    var colorTwo = document.getElementById("td_text")
+    colorTwo.style.backgroundColor=colorBackground;
+}
+function changeColor(colorFont){
+
+    var colorOne = document.getElementById("text")
+    text.style.color=colorFont;
+
+}
